ffc406a53b2c8eb6901dcf826c2b30f4eebfbba6
W1ndys <w1ndys@outlook.com>
2024-04-28 21:07:25 +0800

    更新

--- update_index.py
+++ update_index.py
@@ -5,11 +5,15 @@ from datetime import datetime
 with open("birthdays.yml", "r") as file:
     birthdays = yaml.safe_load(file)
 
-# 获取今天的日期
-today = datetime.today().strftime("%Y-%m-%d")
+# 获取今天的月份和日期
+today = datetime.today().strftime("%m-%d")
 
 # 查找当日过生日的姓名
-birthday_names = [person["name"] for person in birthdays if person["birthday"] == today]
+birthday_names = [
+    person["name"]
+    for person in birthdays
+    if datetime.strptime(person["birthday"], "%Y-%m-%d").strftime("%m-%d") == today
+]
 
 # 将姓名列表转换为逗号分隔的字符串
 names_str = ", ".join(birthday_names)
