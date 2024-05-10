a1b6b21429749330074dbdd3305c13fe1137e01c
W1ndys <w1ndys@Outlook.com>
2024-05-10 10:54:02 +0800

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,6 @@
 import yaml
 from datetime import datetime
 import pytz
-from lunardate import LunarDate
 
 # 设置时区为东八区
 timezone = pytz.timezone('Asia/Shanghai')
@@ -81,11 +80,3 @@ with open('happy-birthday.html', 'w') as file:
 
 # 打印当前日期时间（东八区时间）
 print("当前日期时间：", current_datetime.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
-
-# 获取农历日期
-current_lunar_date = LunarDate.fromSolarDate(current_datetime.year, current_datetime.month, current_datetime.day)
-current_lunar_date_str = current_lunar_date.strftime("%Y-%m-%d (农历 %Y年%m月%d日)")
-
-# 打印今日日期
-print("今日公历日期：", current_date)
-print("今日农历日期：", current_lunar_date_str)
